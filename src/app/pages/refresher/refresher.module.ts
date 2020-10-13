215bdd4f46f884065344a45c1f5b3d3f702916fb
Ramón Carrasco <ramon.carrasco@gft.com>
2020-10-13 13:18:30 +0200

    ADDED Refresher

---
+++ src/app/pages/refresher/refresher.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { RefresherPageRoutingModule } from './refresher-routing.module';
+
+import { RefresherPage } from './refresher.page';
+import { ComponentsModule } from 'src/app/components/components.module';
+
+@NgModule({
+  imports: [
+    ComponentsModule,
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RefresherPageRoutingModule
+  ],
+  declarations: [RefresherPage]
+})
+export class RefresherPageModule {}
